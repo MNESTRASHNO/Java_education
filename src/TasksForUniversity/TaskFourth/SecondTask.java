b4cab7dbe55ebb1b0943c4993f4e2a9211ade313
Al <123@123.ru>
2024-11-06 23:36:38 +0300

    Ya idiot

--- src/TasksForUniversity/TaskFourth/SecondTask.java
+++ src/TasksForUniversity/TaskFourth/SecondTask.java
@@ -5,22 +5,18 @@ public class SecondTask{
         TrafficLight currentLight = TrafficLight.RED;
 
         while (true) {
-            // Вывод текущего состояния светофора и длительности свечения
             System.out.println("Светофор: " + currentLight + ". Ждать " + currentLight.getDuration() + " секунд.");
 
-            // Ожидание указанное количество секунд
             try {
                 Thread.sleep(currentLight.getDuration() * 1000L); // умножаем на 1000, чтобы перевести в миллисекунды
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
-            // Переключение на следующее состояние
             currentLight = getNextLight(currentLight);
         }
     }
 
-    // Метод для получения следующего состояния светофора
     private static TrafficLight getNextLight(TrafficLight currentLight) {
         switch (currentLight) {
             case RED:
